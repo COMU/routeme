914cd869ea75bd780c3981c87c5024e7e4ccd045
sdemircan <demircan.serhat@gmail.com>
2011-12-14 11:30:22 +0000

    route.views.index login olunmasi icin /email/login e yonlendirildi.

--- playground/routeme/route/views.py
+++ playground/routeme/route/views.py
@@ -2,7 +2,9 @@
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 
-@login_required
 def index(request):
+    if not request.user.is_authenticated():
+        return return HttpResponseRedirect('/email/login')
     return render_to_response("route/index.html")
