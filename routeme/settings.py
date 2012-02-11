00a954080a8c565b7bfaef3c2eedad65071adbe7
= <demircan.serhat@gmail.com>
2012-02-11 19:08:24 +0200

    Email ayarlari eklendi.

--- routeme/settings.py
+++ routeme/settings.py
@@ -17,10 +17,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'routemedb',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD':'',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'routeme.db',                      # Or path to database file if using sqlite3.
+        'USER': 'routeme',                      # Not used with sqlite3.
+        'PASSWORD':'sss',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -133,6 +133,13 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+#mail settings
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'noreplyrouteme@gmail.com'
+EMAIL_HOST_PASSWORD = '111111ssss'
 
 
 #required client id and secret
