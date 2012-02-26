fbfe150f939065e8a8dbf60cc9c064ceb877004a
= <demircan.serhat@gmail.com>
2012-02-27 01:13:13 +0200

    Inbox ta mesaj okumadıysa font u degistirildi.

--- routeme/media/static/js/message.js
+++ routeme/media/static/js/message.js
@@ -1,6 +1,21 @@
+var reduceUnread = function(){
+   	currentValue = parseInt($('#unread').html());
+        $('#unread').html((currentValue -1));
+}
+
+var markAsRead = function (row){
+	row.removeClass('well');
+	row.css('font-weight', function(){ return "normal";});
+        reduceUnread();
+}
+
 $("#messageTable tr").click(function () { 
-            var modalId = "modal" + $(this).attr("id");
-            $.post("/message/markRead/", { "id": modalId }, "html");
-            $("#" + modalId).modal('show');
+        var modalId = "modal" + $(this).attr("id");
+        $.post("/message/markRead/", { "id": modalId }, "html");
+        $("#" + modalId).modal('show');
+        if ( $(this).hasClass('well')){
+            markAsRead($(this));
+	}
 });
 
+ 
