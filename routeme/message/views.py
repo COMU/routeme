acc8db92f6bed5c17595e7a8f8e1095d831cbf3e
= <demircan.serhat@gmail.com>
2012-02-27 01:53:06 +0200

    Mesaj gonderme Eklendi.

--- routeme/message/views.py
+++ routeme/message/views.py
@@ -5,6 +5,9 @@ from models import Message
 from django.utils import simplejson
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from forms import MessageForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 @login_required
 def unread_count(request):
@@ -15,13 +18,26 @@ def unread_count(request):
 
 @login_required
 def inbox(request):
+    if request.method == "POST":
+    	form = MessageForm(request.POST)
+        if form.is_valid():
+   	    to_user = form.cleaned_data['to']
+   	    subject = form.cleaned_data['subject']
+   	    message = form.cleaned_data['message']
+            
+	    to_user = User.objects.get(username = to_user)
+	    Message.objects.create_message(request.user, to_user, subject, message)
+            return HttpResponseRedirect("/message/inbox")
+  
     messages = Message.objects.get_all(request.user)
+    form = MessageForm()
     
     unread_message_count = Message.objects.count_unread(request.user)
     profil = render_to_string("include/profil.html", {'user': request.user, 'unread': unread_message_count})
     
-    return render_to_response("message/inbox.html", {'messages': messages, 'profil':profil})
+    return render_to_response("message/inbox.html", {'messages': messages, 'profil':profil, 'form': form})
 
+@login_required
 def mark_read(request):
     message_id = request.POST["id"]
     message = Message.objects.mark_read(id = message_id[5:])# id basinda modal yazarak geliyor
