b8498a0347264c71466067745d150696cf1ab5ba
Serhat Rıfat Demircan <demircan.serhat@gmail.com>
2012-03-15 17:23:29 +0200

    Facebook app id ve secret degistirildi.

--- routeme/facebook/urls.py
+++ routeme/facebook/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^$', view=home),
 
-    url(r'^login$', view=login, name='facebook_login'),
-    url(r'^login_callback$', view=loginCallback, name='facebook_login_callback'),
-    url(r'^newsfeed$', view=newsfeed),
-    url(r'^logout$', view='django.contrib.auth.views.logout'),
+    url(r'^login/$', view=login, name='facebook_login'),
+    url(r'^login_callback/$', view=loginCallback, name='facebook_login_callback'),
+    url(r'^newsfeed/$', view=newsfeed),
+    url(r'^logout/$', view='django.contrib.auth.views.logout'),
 
 )
