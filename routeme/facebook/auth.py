066b6e96e9d1bcb285b9d880d910ad2da449b1d5
Ahmet Can Kepenek <ahmetcan196@gmail.com>
2012-05-09 00:08:59 +0300

    facebook ile login olurken ad ve soyad alanları cekildi

--- routeme/facebook/auth.py
+++ routeme/facebook/auth.py
@@ -50,7 +50,7 @@ class FacebookBackend:
           fb_mail = fb_profile['email']
           user = backend.login(
             facebook_profile, related_name='facebook_profile',
-            username=fb_profile['email'], email=fb_profile['email'])
+            username=fb_profile['email'],first_name = fb_profile['first_name'], last_name = fb_profile['last_name'], email=fb_profile['email'])
         except:
           import sys
           print "error ", sys.exc_info()[0]
