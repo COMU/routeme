2bc7f3183fe46d04d01d699f55727066ed80a84e
Serhat Rıfat Demircan <demircan.serhat@gmail.com>
2012-05-09 15:15:18 +0300

    kullanıcının konumuna göre rota aramada sorgu değiştirildi.

--- routeme/route/views.py
+++ routeme/route/views.py
@@ -214,7 +214,7 @@ def offerRoute(request):
     lng = request.GET['lng']
    
     point = Point(float(lat),float(lng))
-    route = RouteInformation.objects.filter(route__distance_lt = (point, D(km=10)))
+    route = RouteInformation.objects.filter(route__distance_lt = (point, D(km=10)), date__lte=today.today())
     return render_to_response("route/listRoute.html",{'title':'Driveforme', 'routes':enumerate(route, 1),'map':1, "user":request.user})
 
 @login_required
